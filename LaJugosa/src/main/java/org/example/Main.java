9af0dc547de3f9fe1b908309c6981eee3c745c15
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-09-09 14:45:52 -0500

    cambio

--- LaJugosa/src/main/java/org/example/Main.java
+++ LaJugosa/src/main/java/org/example/Main.java
@@ -1,10 +1,44 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        // Programa para crear un ciclo
+        // Y crear un menú de opciones
+
+        Integer opcionSeleccionada=0;
+        while(opcionSeleccionada !=5) {
+            System.out.println("Menú de opciones ");
+            System.out.println("********************");
+            System.out.println("Digita 1 para sumar➕");
+            System.out.println("Digita 2 para restar ➖");
+            System.out.println("Digita 3 para multiplicar ✖");
+            System.out.println("Digita 4 para dividir ➗");
+            System.out.println("Digita 5 para salir del programa 🔚");
+            Scanner leerTeclsdo=new Scanner(System.in);
+            System.out.println("Seleccione una opción por favor");
+            opcionSeleccionada=leerTeclsdo.nextInt();
+
+            // Evaluando Caminos segun el usuario
+            if(opcionSeleccionada==1){
+                System.out.println("Sumando...");
+            }else if (opcionSeleccionada==2){
+                System.out.println("Restando...");
+            }else if (opcionSeleccionada==3){
+                System.out.println("Multiplicando...");
+            }else if (opcionSeleccionada==4){
+                System.out.println("Dividiendo");
+            }else if (opcionSeleccionada==5){
+                System.out.println("Saliendo...");
+
+            }else{
+                System.out.println("Opción invalida");
+            }
+
+        }
 
     }
 }
